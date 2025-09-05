22078e7eccd8918974c70835ce5b56cb40e70204
yvanol <rikamyvanol@gmail.com>
2025-09-05 16:19:08 +0100

    update

--- app/api/order/stripe/route.js
+++ app/api/order/stripe/route.js
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2025-08-27.basil",
 });
 
 export async function POST(request) {
