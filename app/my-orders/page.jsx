e28a8e79d335cfa1a7e5e7a168c370e90da603d1
Rikam Yvanol Giovani <85713319+yvanol@users.noreply.github.com>
2025-09-12 15:31:45 +0100

    Update page.jsx

--- app/my-orders/page.jsx
+++ app/my-orders/page.jsx
@@ -101,11 +101,11 @@ const MyOrders = () => {
                   </p>
                   <div>
                     <p className="flex flex-col">
-                      <span>Method: COD</span>
+                      <span>Method: {order.paymentType || 'COD'}</span>
                       <span>
                         Date: {new Date(order.date).toLocaleDateString()}
                       </span>
-                      <span>Payment: Pending</span>
+                      <span>Payment: {order.isPaid ? 'Paid' : 'Pending'}</span>
                     </p>
                   </div>
                 </div>
