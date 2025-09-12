3d465e3bba3a9a87bbbb8c0e06136f004677a50a
Rikam Yvanol Giovani <85713319+yvanol@users.noreply.github.com>
2025-09-12 15:57:14 +0100

    Update page seller  status order

--- app/seller/orders/page.jsx
+++ app/seller/orders/page.jsx
@@ -100,11 +100,11 @@ const Orders = () => {
                 </p>
                 <div>
                   <p className="flex flex-col">
-                    <span>Method: COD</span>
+                    <span>Method: {order.paymentType || 'COD'}</span>
                     <span>
                       Date: {new Date(order.date).toLocaleDateString()}
                     </span>
-                    <span>Payment: Pending</span>
+                    <span>Payment: {order.isPaid ? 'Paid' : 'Pending'}</span>
                   </p>
                 </div>
               </div>
